e658ef4e31a9370b64af870fe71edcd866ae4160
Martin Cook <33993749+martycook@users.noreply.github.com>
2021-10-22 15:40:03 -0500

    initial commit

---
+++ BlazorApp1/Controllers/AccountsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BlazorApp1.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System;
+using System.Text;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorApp1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        // POST api/<AccountsController>
+        [HttpPost("Login")]
+        public IActionResult Post([FromBody] LoginRequest request)
+        {
+            if (request.SecurityKey != "codegator" ||
+                request.AccountKey != "martin")
+            {
+                return Unauthorized();
+            }
+
+            var securityKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("ThisismySecretKey"));
+            
+            var credentials = new SigningCredentials(
+                securityKey, SecurityAlgorithms.HmacSha256
+                );
+
+            var securityToken = new JwtSecurityToken(
+                issuer: "MyIssuer",
+                audience: "MyAudience",
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials
+                );
+
+            var handler = new JwtSecurityTokenHandler();
+            var jwt = handler.WriteToken(securityToken);
+
+            return Ok(new { authToken = jwt });
+        }
+    }
+}
---
+++ BlazorApp1/Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp1.Models
+{
+    public class LoginRequest
+    {
+        public string AccountKey { get; set; }
+        public string SecurityKey { get; set; }
+    }
+}
